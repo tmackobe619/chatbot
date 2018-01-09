16746d0d3e29f3a8886214c72f2d4b7dc89bc229
tmackobe619 <35244636+tmackobe619@users.noreply.github.com>
2018-01-09 11:46:55 +0800

    123456

--- receive.php
+++ receive.php
@@ -5,6 +5,4 @@ $json_obj=json_decode($json_str); //轉成json格式
 $myfile = fopen("log.txt","w+") or die("Unable to open file");  //設定LOG印訊息
 fwrite($myfile,"\xEF\xBB\xBF".$json_str);//字串前加入"\xEF\xBB\xBF"轉成UTF-8格式
 fclose($myfile);
- 
-
 ?>

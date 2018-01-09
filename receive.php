810a5a0d3617b3bae167c40870cd723015c23c7e
tmackobe619 <35244636+tmackobe619@users.noreply.github.com>
2018-01-09 11:36:36 +0800

    get line msn

--- receive.php
+++ receive.php
@@ -1,3 +1,9 @@
 <?php
-echo "hello Ray"
+$json_str=file_get_contents('php://input');//接收REQUEST 的BODY
+$json_obj=json_decode($json_str); //轉成json格式
+
+$myfile = fopen("log.txt","w+") or die("Unable to open file");  //設定LOG印訊息
+fwrite($myfile,"\xEF\xBB\xBF".$json_str);//字串前加入"\xEF\xBB\xBF"轉成UTF-8格式
+fclose($myfile);
+
 ?>

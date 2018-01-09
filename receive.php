9d924d17f2bd3a6a3e43efe5508d374f99278950
tmackobe619 <35244636+tmackobe619@users.noreply.github.com>
2018-01-09 13:54:38 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -15,7 +15,7 @@ $json_obj=json_decode($json_str); //轉成json格式
 		);
 
 $myfile = fopen("log.txt","w+") or die("Unable to open file");  //設定LOG印訊息
-fwrite($myfile,"\xEF\xBB\xBF".$json_str);//字串前加入"\xEF\xBB\xBF"轉成UTF-8格式
+fwrite($myfile,"\xEF\xBB\xBF".json_encode($response));//字串前加入"\xEF\xBB\xBF"轉成UTF-8格式
 fclose($myfile);
 
  //回傳給line server

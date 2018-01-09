c65275e5fb2955e019180aaf1983ae6e2b8da0a9
tmackobe619 <35244636+tmackobe619@users.noreply.github.com>
2018-01-09 14:32:20 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -21,7 +21,7 @@ fclose($myfile);
 
  //回傳給line server
  $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer WzZtuOblOl7l+EXV6bamontyxP2/RBGD7L78qyFyUxrV+SYfYYJihgkgWNn+YIcaFuXaty2pvJ9lRUpZ6d4BKEyAbM4SFb3+561BWJDMAvGYJQoTq0a32j7yN5yF6/qt0nlGDh3PCJzRPL9WcVw39QdB04t89/1O/w1cDnyilFU=";
+ $header[] = "Authorization: Bearer LDGfUJRlZUajOW7nrqgyvlZTPPUh3Gm7rjE4zPOtRgqGz/AJ/aAKwp827fKbkp7fFEH/zU3cPuWmpzy6Kd8ijDH9tfDYpl3jsB8KOmiR32AnZyxSY294S7+t1La71R1qPYKhj31uogb1bjhblm4s0AdB04t89/1O/w1cDnyilFU=";
  $ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
